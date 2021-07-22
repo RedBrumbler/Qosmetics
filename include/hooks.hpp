b6a57712253887b8f178eaa93a8659a43683cba2
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-07-22 23:07:48 +0200

    forgot the header that made the cool hook thing work

---
+++ include/hooks.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include "beatsaber-hook/shared/utils/logging.hpp"
+
+class Hooks
+{
+    private:
+        static std::vector<void (*)(Logger&)> installFuncs;
+    public:
+        static void AddFunc(void (*installFunc)(Logger& logger))
+        {
+            installFuncs.push_back(installFunc);
+        }
+
+        static void InstallHooks(Logger& logger)
+        {
+            for (auto installFunc : installFuncs)
+            {
+                installFunc(logger);
+            }
+        }
+};
+
+#define QOS_INSTALL_HOOKS(func) \
+struct __QosRegister##func { \
+    __QosRegister##func() { \
+        Hooks::AddFunc(func);\
+    }\
+}; \
+\
+static __QosRegister##func __QosRegisterInstance##func;
+
+#define SIMPLE_INSTALL_HOOK(identifier) INSTALL_HOOK(logger, identifier);
