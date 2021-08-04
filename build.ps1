ae6769c1662b66975209d17cbd8aadf1c0248e95
RedBrumbler <redbrumbler.official@gmail.com>
2021-08-04 14:32:42 +0200

    Update CreatorCache.json

--- build.ps1
+++ build.ps1
@@ -5,7 +5,14 @@ $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 $buildScript = "$NDKPath/build/ndk-build"
 
 $OS = $PSVersionTable.OS
-$windows = $OS.Contains("Windows")
+if ($OS)
+{
+    $windows = $OS.Contains("Windows")
+}
+else
+{
+    $windows = 'True'
+}
 
 # when core, or when on windows we want to add .cmd to the end
 if ((-not ($PSVersionTable.PSEdition -eq "Core")) -or $windows) {
@@ -29,11 +36,18 @@ else
 # if all fails, just assume 4
 if (-not $coreCount)
 {
+    echo "Couldn't get core count, assuming 4"
     $coreCount = 4
 }
 
 $msg = "Building with " + $coreCount + " Cores!"
 echo $msg
+if ($p1 -eq "clean")
+{
+    echo "Removing obj dir for clean build"
+    remove-item ./obj -Recurse
+}
+
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j $coreCount -Oline
 
 if (-not ($LastExitCode -eq 0)) {
