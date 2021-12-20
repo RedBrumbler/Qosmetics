cdb6f93ab6368b049c2b9d1bdfab3653ccc99c13
RedBrumbler <redbrumbler.official+github@gmail.com>
2021-12-21 00:19:37 +0100

    Convert to cmake + qpm-rust

--- buildQmod.ps1
+++
@@ -1,4 +0,0 @@
-& ./build.ps1
-$env:qmodName = "Qosmetics"
-$env:module_id = "questcosmetics"
-& ./CreateQmod.ps1
--- copy.ps1
+++ copy.ps1
@@ -1,5 +1,5 @@
 & ./build.ps1
-& adb push libs/arm64-v8a/libquestcosmetics.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libquestcosmetics.so
+& adb push build/libquestcosmetics.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libquestcosmetics.so
 & Start-Sleep -Milliseconds 500
 & adb shell am force-stop com.beatgames.beatsaber
 & adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
